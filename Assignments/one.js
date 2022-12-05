b888ebfd2649033f471e9e71e19ecc3d7d4e1549
guduguntlasaitharun <guduguntlasaitharun@gmail.com>
2022-12-05 18:15:34 +0530

    practice

---
+++ Assignments/one.js
@@ -0,0 +1,28 @@
+let arry=[10,20,30,40]
+let arrsum=0;
+arry.forEach(Element=>{
+    arrsum=arrsum+Element;
+})
+console.log(arrsum);
+
+/*
+let arry=[[10,20,30],[10,40,50],[10,80,20]]
+let arrsum=0;
+for(let i=0;i<arry.length;i++){
+arry[i].forEach(Element=>{
+    arrsum=arrsum+Element;
+});
+}
+console.log(arrsum);
+/*
+
+let arry=[[[10,20,30]],[[10,40,50]],[[10,80,20]]]
+let arrsum=0;
+for(let i=0;i<arry.length;i++){
+arry[i].forEach(Element=>{
+    arrsum=arrsum+Element;
+})
+console.log(arrsum);
+
+}
+*/
---
+++ Assignments/two.js
@@ -0,0 +1,5 @@
+let cust_data=[{"id":1,"first_name":"Axel","email":"ailiffe0@ftc.gov","location":"Damanhūr"},
+{"id":2,"first_name":"Gleda","email":"gdorgan1@upenn.edu","location":"Chengjiao"},
+{"id":99,"first_name":"Justinn","email":"jbishop2q@weebly.com","location":"Łazy"},
+{"id":100,"first_name":"Lodovico","email":"lrustman2r@ifeng.com","location":"Nasīrābād"}]
+console.log(cust_data[3].location);
