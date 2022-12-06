c011a311e32112e20b12c692765ab5561cb4f358
guduguntlasaitharun <guduguntlasaitharun@gmail.com>
2022-12-06 12:49:05 +0530

    onclick

---
+++ Assignments/DOM/one.js
@@ -0,0 +1,20 @@
+function changeColor() {
+    //console.log("Test")
+    //let ref = document.getElementById('btn').style.backgroundColor = "green"
+    document.getElementById('btn').style.backgroundColor = "green"
+    //console.log(ref)
+    //ref.style.backgroundColor = "green"
+}
+
+
+function apple() {
+    //console.log("Test")
+    let ref = document.getElementById('abc').style.backgroundColor = "orange"//let  not declare also run
+   // console.log(ref)
+    //ref.style.backgroundColor = "orange"
+}
+
+function ball(){
+    document.getElementById('sai').style.backgroundColor = "yellow"
+
+}
