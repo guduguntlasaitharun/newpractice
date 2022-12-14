9d84961a917b4258afe756173858cbe650fb3819
guduguntlasaitharun <guduguntlasaitharun@gmail.com>
2022-12-14 23:46:46 +0530

    inhertance

---
+++ iterative/units.js
@@ -0,0 +1,9 @@
+let a="234"
+let number=a.length;
+if(a[number-1]==="4")
+{
+    console.log("the given number have 4 in units");
+}
+else{
+    console.log("the given number doesnt have 4 in units place");
+}
