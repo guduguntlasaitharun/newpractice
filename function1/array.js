ca30df63058cce2f85de8eda5d8bda04c010eb76
guduguntlasaitharun <guduguntlasaitharun@gmail.com>
2022-12-17 23:45:52 +0530

    ok

--- function1/array.js
+++ function1/array.js
@@ -1,3 +1,11 @@
 let list=[{id:101,name:"sai",salary:4500},{id:102,name:"tharun",salary:400},{id:103,name:"aryan",salary:450}]
 console.log(list[0].id);
 console.log(list[3].name);
+
+/*
+Object
+let list={id:[101,102,103],name:["sai","tharun","ARYAN"],sal:[400,500,600]}
+console.log(list.id[0]);
+console.log(list.name[1]);
+*/ 
+console.log(list);
