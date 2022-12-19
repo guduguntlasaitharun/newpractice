d4f574a4bb94065c0b93464e99b89f4efacccee7
guduguntlasaitharun <guduguntlasaitharun@gmail.com>
2022-12-19 22:00:39 +0530

    s

--- ES-6/ele.js
+++ ES-6/ele.js
@@ -1,4 +1,8 @@
-let enames={enames_id:101,enames_name:"sai",enames_email:"saitharun@gmail.com"}
+/*let enames={enames_id:101,enames_name:"sai",enames_email:"saitharun@gmail.com"}
 let details={salary:50000,location:"gdlr",company:"Wipro"}
 let result={...enames,...details}
-console.log(result);
+console.log(result);*/
+
+let green;
+grnee=false;
+console.log(grnee);
